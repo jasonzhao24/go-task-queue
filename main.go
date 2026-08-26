c57a7e4eae3754ab42b1d551e5ece66a12cc8e1a
jasonzhao24 <jasonzhao420@gmail.com>
2026-08-25 20:39:47 -0400

    Enqueue function

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 	"time"
 )
@@ -26,6 +27,31 @@ type MemoryQueue struct {
 	tasks map[string]*Task
 }
 
+func NewMemoryQueue() *MemoryQueue {
+	return &MemoryQueue{
+		tasks: make(map[string]*Task),
+	}
+}
+
+// # adds task to queue and return ID
+func (q *MemoryQueue) Enqueue(payload []byte) string {
+	q.mu.Lock()
+	// makes the mutex unlock when the function finishes
+	defer q.mu.Unlock()
+
+	id := fmt.Sprintf("%d", time.Now().UnixNano())
+
+	newTask := &Task{
+		ID:         id,
+		Payload:    payload,
+		State:      Available,
+		RetryCount: 0,
+	}
+	// Saves to map
+	q.tasks[id] = newTask
+
+	return id
+}
 func main() {
 
 }
